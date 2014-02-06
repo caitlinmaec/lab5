2d26b24071c8afa8f3ed44b58d388ddb7dbe47e6
vagrant <vagrant@precise32.(none)>
2014-02-06 18:51:43 +0000

    putting it together lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -5,11 +5,13 @@ $(document).ready(function() {
 	initializePage();
 })
 
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	console.log("Javascript connected!");
+
 }
 
 function anagrammedName(name) {
